b44cff2976be7a71bb802bf16b971a1730ca29ee
Roman Shestopal <romchyk.r@gmail.com>
2023-06-15 17:06:41 +0300

    Initial Commit

---
+++ SwiftUI-Apprentice-2023/SwiftUI_Apprentice_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Apprentice_2023App.swift
+//  SwiftUI-Apprentice-2023
+//
+//  Created by Roman Shestopal on 15.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Apprentice_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
